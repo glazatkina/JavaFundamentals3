0b2b7b663a5e8a7dabbc4f380a35c95c5bd2d32d
glazkina <glazatkina@gmail.com>
2016-10-19 13:28:46 +0300

    task 3 from module input-output

---
+++ io/src/main/java/EncodingIO.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.nio.charset.Charset;
+
+/**
+ * Created by glazkina on 19.10.2016.
+ *
+ * @author: Natalia Glazkina
+ */
+public class EncodingIO {
+    public static void main(String[] args) {
+        try (FileInputStream fileReader = new FileInputStream("io\\src\\main\\resources\\testUTF8.txt");
+             FileOutputStream fileWriter = new FileOutputStream("io\\src\\main\\resources\\testUTF16.txt");
+             InputStreamReader inputStreamReader = new InputStreamReader(fileReader, Charset.forName("UTF8"));
+             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileWriter, Charset.forName("UTF16"))
+        ) {
+            StringBuilder stringBuilder = new StringBuilder();
+            int next;
+            while ((next = inputStreamReader.read()) >= 0) {
+                stringBuilder.append((char)next);
+            }
+            outputStreamWriter.write(stringBuilder.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
