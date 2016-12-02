5f428e17ffc3da747de7783f7605cb9396c85712
glazkina <glazatkina@gmail.com>
2016-12-02 19:01:11 +0300

    task 1 from module collections

--- collections/src/main/java/UniversalPropertiesMap.java
+++ collections/src/main/java/UniversalPropertiesMap.java
@@ -1,6 +1,6 @@
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by glazkina on 28.11.2016.
@@ -9,15 +9,21 @@ import java.util.Map;
  */
 public class UniversalPropertiesMap {
     public static void main(String[] args) {
-        Map<String, String> properties = readPropertyFile("");
+        Map<String, String> properties = readPropertyFile("C:\\Users\\glazkina\\IdeaProjects\\JavaFundamentals3\\strings\\src\\main\\resources\\LocalizationResources_en.properties");
+        System.out.println(properties);
     }
 
+    @SuppressWarnings("WeakerAccess")
     static Map<String, String> readPropertyFile(String fileName) {
+        Map<String,String> result = new HashMap<>();
         try (FileInputStream is = new FileInputStream(fileName)) {
-            return null;
+            ResourceBundle rc = new PropertyResourceBundle(is);
+            for (String key: rc.keySet()) {
+                result.put(key, rc.getString(key));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return result;
     }
 }
